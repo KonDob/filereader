fb67e39c8e4c48dcb1ec27469a206c7cff2ff106
KonDob <konstantin.dobro@gmail.com>
2020-11-05 21:37:08 +0200

    Initial commit with some basic

---
+++ main_script/outputlines.py
@@ -0,0 +1,39 @@
+from os import path
+
+path_to_file = input('Please provide location of target file: ')
+number = input('Please add amount for lines: ')
+
+
+
+def print_paths_parts(file, number):
+
+
+
+    """
+        This method outputs print b amount of parts of input path to file in correct order.
+        Amount of parts also an input argument
+        Works on with all common type of path : Unix and Win
+
+        :parameters:
+            path : string - path to be sliced
+            number : int -  amount of last lines of file
+
+        :returns :
+            None
+    """
+    number = int(number)
+    
+    if path.isfile(path_to_file):
+        print('Path file is valid. Yeah this is path to file' + '\n')
+        with open(file, 'r') as file:
+            file_lines = file.readlines()
+            for i in reversed(range(1, number+1)):
+            # Print last N lines of file in correct order
+                print(file_lines[-i])
+    else:
+        print('Sorry - path to file doesn`t exist. Please provide another file')
+
+        
+
+
+print_paths_parts(path_to_file, number)
---
+++ tests/tests.py
@@ -0,0 +1,33 @@
+import pytest
+
+from outputlines import print_paths_parts
+
+
+def test_check_output_last_line(capsys):
+    print_paths_parts('test_file.txt', 1)
+    captured = capsys.readouterr()
+    assert captured.out == "end\n\n"
+
+
+def test_check_return_data():
+    assert print_paths_parts('test_file.txt', 2) == None
+
+
+def test_empty_call():
+    with pytest.raises(TypeError):
+        print_paths_parts()
+
+
+def test_call_only_filename():
+    with pytest.raises(TypeError):
+        print_paths_parts('some.txt')
+
+
+def test_call_only_number():
+    with pytest.raises(TypeError):
+        print_paths_parts(2)
+
+
+def test_file_not_found():
+    with pytest.raises(FileNotFoundError):
+        print_paths_parts('none.txt', 2)
